10dbb970b1c50f6e49a979086ec6bc5bde18b660
DhanushM12 <dhanushmukhi@gmail.com>
2023-04-23 14:07:21 +0530

    Added chat app

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ const users = {};
 
 io.on('connection', socket => {
     socket.on('new-user', name => {
-        console.log(name)
+        // console.log(name)
         users[socket.id] = name
         socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message =>{
+        socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-diconnected', users[socket.id])
+        delete users[socket.id]
     })
 })
